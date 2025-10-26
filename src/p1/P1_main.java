2b8dae7723f25ce2e13889f02b71d4d8ca52118c
Marcel Gaub <mgaub@smail.uni-koeln.de>
2025-10-26 23:17:19 +0100

    commt

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -1,4 +1,11 @@
 package p1;
+import java.util.Scanner;
 
 public class P1_main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String zeile = s.nextLine();
+        System.out.println(zeile);
+        s.close();
+    }
 }
