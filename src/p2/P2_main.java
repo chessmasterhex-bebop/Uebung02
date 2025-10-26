abf269215c5c69d4e4f6f122c631ec83d9516e3b
Marcel Gaub <mgaub@smail.uni-koeln.de>
2025-10-26 23:24:52 +0100

    P2

---
+++ src/p2/P2_main.java
@@ -0,0 +1,18 @@
+package p2;
+
+public class P2_main {
+    public static void main(String[] args) {
+        // Variablen deklarieren und initialisieren
+        int i = 8;         // beliebiger Startwert
+        boolean b = false; // beliebiger Startwert
+
+        // Bedingung prüfen
+        if (i > 10) {
+            b = true;
+        }
+
+        // Ergebnis ausgeben
+        System.out.println("i = " + i);
+        System.out.println("b = " + b);
+    }
+}
