9081a6930f17377dd5334326e6d0ff62793c9d91
Marcel Gaub <mgaub@smail.uni-koeln.de>
2025-10-26 23:27:29 +0100

    P3

---
+++ src/p3/P3_main.java
@@ -0,0 +1,25 @@
+package p3;
+
+public class P3_main {
+    public static void main(String[] args) {
+        // Drei beliebige Werte
+        int i = 1;
+        int j = 2;
+        int k = 3;
+
+        // Vergleiche
+        if (i < j) {
+            System.out.print("A");
+        }
+
+        if (i < k) {
+            System.out.print("B");
+        }
+
+        if (j < k) {
+            System.out.print("C");
+        }
+
+        System.out.println(); // Zeilenumbruch am Ende
+    }
+}
